9690d1860a220034d2cbabffcc1a24d7fc767cfc
Anatoly Evsyukov <yevsyukof@gmail.com>
2022-07-21 07:17:48 +0700

    Написал часть бэкэнда, добавил сборочные файлы

---
+++ monoBackend/cmd/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"said-and-dot-backend/internal/config"
+	"said-and-dot-backend/internal/database"
+	"said-and-dot-backend/internal/logger"
+	"said-and-dot-backend/internal/server"
+)
+
+var (
+	dotEnvConfigFilePath string
+	preforkStatus        bool
+)
+
+func init() {
+	flag.StringVar(&dotEnvConfigFilePath, "config", "./configs/.env", "The application configurations")
+	flag.BoolVar(&preforkStatus, "prefork", false, "Run the app in Prefork mode [multiple Go processes]")
+}
+
+func main() {
+	flag.Parse()
+
+	//log.Println("APP_HOST =", config.GetString("APP_HOST", "127.0.0.1"))
+	//log.Println()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := config.Load(dotEnvConfigFilePath); err != nil {
+		log.Fatal(err)
+	}
+
+	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		config.GetString("DB_USER", "postgres"),
+		config.GetString("DB_PASSWORD", "postgres"),
+		config.GetString("DB_HOST", "127.0.0.1"),
+		config.GetInt("DB_PORT", 5432),
+		config.GetString("DB_DATABASE", "snd-data"),
+	)
+
+	dbInstance, err := database.New(ctx, dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	serverInstance := server.New(
+		dbInstance,
+		logger.NewLogger(config.GetBool("DEBUG", false)),
+		&server.Config{
+			AppName: config.GetString("APP_NAME", "monoBackend"),
+			Host:    config.GetString("APP_HOST", "127.0.0.1"),
+			Port:    config.GetString("APP_PORT", "5000"),
+		},
+		fiber.Config{
+			Prefork: preforkStatus,
+		},
+	)
+
+	serverInstance.Listen()
+}
---
+++ monoBackend/go.mod
@@ -0,0 +1,35 @@
+module said-and-dot-backend
+
+go 1.18
+
+require (
+	github.com/gofiber/fiber/v2 v2.35.0
+	github.com/jackc/pgx/v4 v4.16.1
+	github.com/joho/godotenv v1.4.0
+	github.com/mattn/go-colorable v0.1.6
+	go.uber.org/zap v1.13.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/puddle v1.2.1 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.38.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114 // indirect
+)
